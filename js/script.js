e345f66fef394259c843e9aaafdbab4d28054fa8
syuhada-ramadhan <suhadahada18@gmail.com>
2021-01-07 20:08:32 +0700

    Latihan Mencari nilai Tertinggi selesai

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// array nomor
+const nomor = [1, 4, 5, 8, 10, 30, 50, 100];
+
+// cek nilai tertinggi pada array nomor
+const nilaiTertinggi = Math.max(...nomor);
+
+// cetak variabel nilai Tertinggi;
+console.log(nilaiTertinggi);
